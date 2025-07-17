c8a86be058951885781c22318a17b8440668a653
Lucas Sutherland <sutherlandlucas1@gmail.com>
2025-07-17 21:57:57 +1000

    no longer virus

--- scripts/lil_dude.gd
+++ scripts/lil_dude.gd
@@ -6,19 +6,24 @@ extends CharacterBody3D
 const SPEED = 10.0
 const JUMP_VELOCITY = 8.0
 const MOUSE_SENSITIVITY = 0.005
+var yoinked_mouse = true;
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	#camera.current = true
 
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and yoinked_mouse:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, -PI / 2, PI / 2)
 		
 	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
+		if (yoinked_mouse):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		yoinked_mouse = !yoinked_mouse
 
 func _physics_process(delta):
 	# Add the gravity.
